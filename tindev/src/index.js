ee7e220c2556c41a13c3095c822da3bfbfb1def3
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2019-12-02 15:23:23 -0300

    Mobile part

---
+++ tindev/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'; 
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
